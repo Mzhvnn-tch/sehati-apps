0a6dceccf441fa87fea543e5de6b3412489f5be8
mzhvnn-tch <mofalo.granaro@gmail.com>
2026-05-29 17:58:09 +0700

    chore: clean up debugging and database scripts

--- push-db.cjs
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  console.log("Pushing DB...");
-  execSync("npx drizzle-kit push", { stdio: "inherit", input: "y\ny\ny\n" });
-  console.log("Done");
-} catch (e) {
-  console.error(e.message);
-}
